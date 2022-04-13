ec96a5ef42d6ad274e332f705802685aecbd1bfe
Jacob Olshanskiy <101607869+JacobProgrammer@users.noreply.github.com>
2022-04-13 12:50:02 -0700

    Update home.py

--- home.py
+++ home.py
@@ -1,5 +1,7 @@
 """
 This program takes user information and outputs the optimal area to buy a home
+If user puts in value of budget less that 1000000...
+Do not include the 0's after number (ex -> 900,000 = 900)
 
 Author: Jacob Olshanskiy
 Date:4-12-22
@@ -61,8 +63,35 @@ if homestate == " Colorado" or homestate ==" colorado":
     greeting()
     price6 = price()
     if price6 > 1000000:
-        print("The optimal city for you to shop is ")
+        print("The optimal city for you to shop is Aspen/Vail")
     elif 999 > price6 > 500:
-        print("The optimal city for you to shop is ")
+        print("The optimal city for you to shop is Denver")
     else:
-        print("The optimal are for you to shop is ")
+        print("The optimal are for you to shop is Lakewood")
+if homestate == " Connecticut" or homestate == " connecticut":
+    greeting()
+    price7 = price()
+    if price7 > 1000000:
+        print("The optimal city for you to shop is Greenwich")
+    elif 999 > price7 > 500:
+        print("The optimal city for you to shop is Ridgefield")
+    else:
+        print("The optimal are for you to shop is Trumbull")    
+if homestate == " Delaware" or homestate == " delaware":
+    greeting()
+    price8 = price()
+    if price8 > 1000000:
+        print("The optimal city for you to shop is Bethany Beach")
+    elif 999 > price8 > 500:
+        print("The optimal city for you to shop is Rehoboth Beach")
+    else:
+        print("The optimal are for you to shop is Lewes")
+if homestate == " Florida" or  homestate == " florida":
+    greeting()
+    price9 = price()
+    if price9 > 1000000:
+        print("The optimal city for you to shop is Boca Raton/Palm Beach")
+    elif 999 > price9 > 500:
+        print("The optimal city for you to shop is Highland Beach")
+    else:
+        print("The optimal are for you to shop is Sunny Isles")
