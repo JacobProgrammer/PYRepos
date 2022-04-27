815545e9aa4628f4a6d1d0552970355e32779522
Jacob Olshanskiy <101607869+JacobProgrammer@users.noreply.github.com>
2022-04-27 11:05:54 -0700

    Add files via upload

---
+++ HatChoice.py
@@ -0,0 +1,9 @@
+"""
+This program randomly selects and outputs an object in a list
+
+Author: Jacob Olshanskiy
+Date: 4-26-22
+"""
+import random
+hats = ['Alo', 'JSC', 'SpaceX', 'UTHR', 'FIRST CMP', 'Plain Gray']
+print(random.choice(hats))
