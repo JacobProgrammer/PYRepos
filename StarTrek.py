ade1de0ced6d8a79383854efdf6f471f8a053229
Jacob Olshanskiy <101607869+JacobProgrammer@users.noreply.github.com>
2022-04-28 13:06:26 -0700

    Add files via upload

--- StarTrek.py
+++ StarTrek.py
@@ -4,3 +4,21 @@ This program demosntrates the use of dictionaries to preform outputs with the th
 Author:Jacob Olshanskiy
 Date: 4-26-22
 """
+captains = {
+    "Enterprise": "Picard",
+    "Voyager": "Janeway",
+    "Defiant": "Sisko",
+}
+if "Enterprise" in captains:
+    print("Found")
+else:
+    captains["Discovery"] = ["Unknown"]
+print(captains)
+if "Discovery" in captains:
+    print("Found")
+else:
+    captains["Discovery"] = ["Unknown"]
+for pilots in captains:
+    print("The", pilots, "is piloted by", captains[pilots])
+captains.pop("Discovery")
+print(captains)
